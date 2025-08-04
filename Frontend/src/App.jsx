b25c1e94e688820ced1f1480b4f22b3a9607ab2a
Ala111205 <sadham070403@gmail.com>
2025-08-04 13:23:29 +0530

    cn

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {BrowserRouter as Router, Routes, Route, BrowserRouter} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from "./components/navbar.jsx";
 import Home from "./pages/home.jsx";
 import './App.css';
@@ -17,21 +17,19 @@ function App() {
   return (
     <>
       <div className="container">
-        <BrowserRouter basename="/">
-          <Router>
-            <Navbar show={show} setShow={setShow}/>
-            <Routes>
-              <Route path="/" element={<Home/>} />
-              <Route path="/enroll" element={<Enrollment/>} />
-              <Route path="/studentRegister" element={<StudentRegister/>} />
-              <Route path="/teacherRegister" element={<TeacherRegister/>} />
-              <Route path="/adminApproval" element={<AdminApproval/>} />
-              <Route path="/studentData" element={<Studentdata/>} />
-              <Route path="/teacherData" element={<TeacherData/>} />
-              <Route path="/contact" element={<Contact/>} />
-            </Routes>
-          </Router>
-        </BrowserRouter>
+        <Router>
+          <Navbar show={show} setShow={setShow}/>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/enroll" element={<Enrollment/>} />
+            <Route path="/studentRegister" element={<StudentRegister/>} />
+            <Route path="/teacherRegister" element={<TeacherRegister/>} />
+            <Route path="/adminApproval" element={<AdminApproval/>} />
+            <Route path="/studentData" element={<Studentdata/>} />
+            <Route path="/teacherData" element={<TeacherData/>} />
+            <Route path="/contact" element={<Contact/>} />
+          </Routes>
+        </Router>
       </div>
     </>
   )
