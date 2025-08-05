bdb074261c3dfcc8ffdec4b79479d28272c8fe50
Ala111205 <sadham070403@gmail.com>
2025-08-05 15:55:36 +0530

    gd

--- Frontend/src/axios.js
+++ Frontend/src/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-     baseURL: process.env.REACT_APP_API_URL,
+     baseURL: import.meta.env.VITE_API_URL,
      withCredentials: true
 });
 
-console.log(".env file Not working: ",process.env.REACT_APP_API_URL)
+console.log(".env file Not working: ",import.meta.env.VITE_API_URL)
 
 export default api;
